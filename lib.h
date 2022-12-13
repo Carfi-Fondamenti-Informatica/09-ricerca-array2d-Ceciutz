5b5c17c0fae6a7300801d5466e0bab2acec14ac4
Ceciutz <115075907+Ceciutz@users.noreply.github.com>
2022-12-13 12:53:36 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -2,7 +2,7 @@
 // Created by Cecilia Utzeri on 13/12/2022
 //
 
-#ifndef ESERCITAZIONE_9_LIB_H
-#define ESERCITAZIONE_9_LIB_H
+#ifndef ESERCITAZIONE_9
+#define ESERCITAZIONE_9
 bool cerca(char[10][20], int &, char []);
 #endif //ESERCITAZIONE_9_LIB_H
