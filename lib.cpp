494ab451c836e942abb27fbe80bac37071d59368
Ceciutz <115075907+Ceciutz@users.noreply.github.com>
2022-12-13 12:55:47 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -3,10 +3,11 @@
 //
 
 
-bool cerca(char lista [10][20], int&posizione, char target[]){
+bool cerca(char lista [10][20], int&posizione,
+char target[]){
   for (int i=0; i<10;i++){
     for (int j=0; j<20; j++){
-      if (lista[i][j] != target[j]{
+      if (lista[i][j] != target[j]){
           j=20;
     }else {
         if(j==19){
@@ -14,8 +15,8 @@ bool cerca(char lista [10][20], int&posizione, char target[]){
           return true;
         }
       }
-          }
-          }
+    }
+  }
           return false;
           }
     
